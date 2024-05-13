ce8806c33d4839e7f654a4ee173d7f22bd180f48
Denzelle Walker <dwalker@jetlifetech.com>
2024-05-13 11:11:27 -0500

    feat: fixed route

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -4,7 +4,7 @@ import { HttpStatusCode } from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
   const id = params.id;
   try {
     const note = await findNoteById(id);
